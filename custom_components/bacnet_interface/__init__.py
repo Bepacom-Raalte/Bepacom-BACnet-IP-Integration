a12dafaf8b921a34b90fb9c18fa3633f01925287
GravySeal <lhwgeertsema@outlook.com>
2024-02-01 14:17:51 +0100

    Added release service, testing hacs

--- custom_components/bacnet_interface/__init__.py
+++ custom_components/bacnet_interface/__init__.py
@@ -1,9 +1,15 @@
 """The Bepacom EcoPanel BACnet/IP integration."""
 from __future__ import annotations
 
+import voluptuous as vol
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.core import (HomeAssistant, ServiceCall, ServiceResponse,
+                                SupportsResponse)
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.device_registry import DeviceEntry, async_get
+from homeassistant.util.json import JsonObjectType
 
 from .const import DOMAIN, LOGGER
 from .coordinator import EcoPanelDataUpdateCoordinator
@@ -17,6 +23,15 @@ PLATFORMS: list[str] = [
     "switch",
     "select",
 ]
+
+WRITE_RELEASE_SERVICE_NAME = "write_release"
+ATTR_PRIORITY = "priority"
+WRITE_RELEASE_SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_ENTITY_ID): cv.entity_ids,
+        vol.Optional(ATTR_PRIORITY): int,
+    }
+)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -37,6 +52,32 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     # Reload entry when its updated.
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+
+    async def write_release(call: ServiceCall) -> ServiceResponse:
+        """Write empty presentValue that serves to release higher priority write request."""
+
+        entity_registry = er.async_get(hass)
+
+        entity_data = entity_registry.async_get(call.data[ATTR_ENTITY_ID][0])
+
+        device_id, object_id = entity_data.unique_id.split("_")
+
+        if call.data.get(ATTR_PRIORITY):
+            LOGGER.warning("Priority is currently not functioning. Writing default value.")
+
+        await coordinator.interface.write_property(
+            deviceid=device_id, objectid=object_id
+        )
+
+        return {"status": "successfull!"}
+
+    hass.services.async_register(
+        DOMAIN,
+        WRITE_RELEASE_SERVICE_NAME,
+        write_release,
+        schema=WRITE_RELEASE_SCHEMA,
+        supports_response=SupportsResponse.OPTIONAL,
+    )
 
     return True
 
