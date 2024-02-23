fa7f4b13f8853187c5204ced15fe0ba3e793e8d0
GravySeal <lhwgeertsema@outlook.com>
2024-02-23 16:41:48 +0100

    V0.1.5 validating config entry

--- custom_components/bacnet_interface/__init__.py
+++ custom_components/bacnet_interface/__init__.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 
 import voluptuous as vol
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.const import (ATTR_ENTITY_ID, CONF_ENABLED, CONF_HOST,
+								 CONF_NAME, CONF_PORT)
 from homeassistant.core import (HomeAssistant, ServiceCall, ServiceResponse,
-                                SupportsResponse)
+								SupportsResponse)
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.device_registry import DeviceEntry, async_get
@@ -17,105 +18,123 @@ from .coordinator import EcoPanelDataUpdateCoordinator
 # List of platforms to support. There should be a matching .py file for each,
 # eg <cover.py> and <sensor.py>
 PLATFORMS: list[str] = [
-    "binary_sensor",
-    "sensor",
-    "number",
-    "switch",
-    "select",
+	"binary_sensor",
+	"sensor",
+	"number",
+	"switch",
+	"select",
 ]
 
 WRITE_RELEASE_SERVICE_NAME = "write_release"
 ATTR_PRIORITY = "priority"
 WRITE_RELEASE_SCHEMA = vol.Schema(
-    {
-        vol.Required(ATTR_ENTITY_ID): cv.entity_ids,
-        vol.Optional(ATTR_PRIORITY): int,
-    }
+	{
+		vol.Required(ATTR_ENTITY_ID): cv.entity_ids,
+		vol.Optional(ATTR_PRIORITY): int,
+	}
 )
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up EcoPanel BACnet/IP interface from a config entry."""
+	"""Set up EcoPanel BACnet/IP interface from a config entry."""
 
-    # Store an instance of the "connecting" class that does the work of speaking
-    # with your actual devices.
+	# Store an instance of the "connecting" class that does the work of speaking
+	# with your actual devices.
 
-    coordinator = EcoPanelDataUpdateCoordinator(hass, entry=entry)
+	validate_entry(entry)
 
-    await coordinator.async_config_entry_first_refresh()
+	coordinator = EcoPanelDataUpdateCoordinator(hass, entry=entry)
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+	await coordinator.async_config_entry_first_refresh()
 
-    # This creates each HA object for each platform your device requires.
-    # It's done by calling the `async_setup_entry` function in each platform module.
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+	hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    # Reload entry when its updated.
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+	# This creates each HA object for each platform your device requires.
+	# It's done by calling the `async_setup_entry` function in each platform module.
+	await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    async def write_release(call: ServiceCall) -> ServiceResponse:
-        """Write empty presentValue that serves to release higher priority write request."""
+	# Reload entry when its updated.
+	entry.async_on_unload(entry.add_update_listener(async_reload_entry))
 
-        entity_registry = er.async_get(hass)
+	async def write_release(call: ServiceCall) -> ServiceResponse:
+		"""Write empty presentValue that serves to release higher priority write request."""
 
-        entity_data = entity_registry.async_get(call.data[ATTR_ENTITY_ID][0])
+		entity_registry = er.async_get(hass)
 
-        device_id, object_id = entity_data.unique_id.split("_")
+		entity_data = entity_registry.async_get(call.data[ATTR_ENTITY_ID][0])
 
-        if call.data.get(ATTR_PRIORITY):
-            LOGGER.warning(
-                "Priority is currently not functioning. Writing default value."
-            )
+		device_id, object_id = entity_data.unique_id.split("_")
 
-        await coordinator.interface.write_property(
-            deviceid=device_id, objectid=object_id
-        )
+		if call.data.get(ATTR_PRIORITY):
+			LOGGER.warning(
+				"Priority is currently not functioning. Writing default value."
+			)
 
-        return {"status": "successfull!"}
+		await coordinator.interface.write_property(
+			deviceid=device_id, objectid=object_id
+		)
 
-    hass.services.async_register(
-        DOMAIN,
-        WRITE_RELEASE_SERVICE_NAME,
-        write_release,
-        schema=WRITE_RELEASE_SCHEMA,
-        supports_response=SupportsResponse.OPTIONAL,
-    )
+		return {"status": "successfull!"}
 
-    return True
+	hass.services.async_register(
+		DOMAIN,
+		WRITE_RELEASE_SERVICE_NAME,
+		write_release,
+		schema=WRITE_RELEASE_SCHEMA,
+		supports_response=SupportsResponse.OPTIONAL,
+	)
+
+	return True
+
+
+def validate_entry(entry: ConfigEntry) -> bool:
+	"""Check if all values are filled in, otherwise replace"""
+
+	if not entry.data.get(CONF_PORT):
+		entry.data.update({CONF_PORT: 8099})
+
+	if not entry.data.get(CONF_ENABLED):
+		entry.data.update({CONF_ENABLED: True})
+
+	if not entry.data.get(CONF_HOST):
+		entry.data.update({CONF_HOST: "127.0.0.1"})
+
+	if not entry.data.get(CONF_NAME):
+		entry.data.update({CONF_NAME: "object_name"})
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    # This is called when an entry/configured device is to be removed. The class
-    # needs to unload itself, and remove callbacks. See the classes for further
-    # details
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        coordinator: EcoPanelDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+	"""Unload a config entry."""
+	# This is called when an entry/configured device is to be removed. The class
+	# needs to unload itself, and remove callbacks. See the classes for further
+	# details
+	if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+		coordinator: EcoPanelDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
-        await coordinator.interface.disconnect()
-        if coordinator.unsub:
-            coordinator.unsub()
+		await coordinator.interface.disconnect()
+		if coordinator.unsub:
+			coordinator.unsub()
 
-        del hass.data[DOMAIN][entry.entry_id]
+		del hass.data[DOMAIN][entry.entry_id]
 
-    return unload_ok
+	return unload_ok
 
 
 async def async_remove_config_entry_device(
-    hass: HomeAssistant, config_entry: ConfigEntry, device_entry: DeviceEntry
+	hass: HomeAssistant, config_entry: ConfigEntry, device_entry: DeviceEntry
 ) -> bool:
-    """Remove config entry from a device."""
-    coordinator: EcoPanelDataUpdateCoordinator = hass.data[DOMAIN][
-        config_entry.entry_id
-    ]
+	"""Remove config entry from a device."""
+	coordinator: EcoPanelDataUpdateCoordinator = hass.data[DOMAIN][
+		config_entry.entry_id
+	]
 
-    for domain, device_id in device_entry.identifiers:
-        coordinator.logger.error(coordinator.data.devices[device_id])
-        coordinator.data.devices.pop(device_id)
+	for domain, device_id in device_entry.identifiers:
+		coordinator.logger.error(coordinator.data.devices[device_id])
+		coordinator.data.devices.pop(device_id)
 
-    return True
+	return True
 
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload the config entry when it changed."""
-    await hass.config_entries.async_reload(entry.entry_id)
+	"""Reload the config entry when it changed."""
+	await hass.config_entries.async_reload(entry.entry_id)
